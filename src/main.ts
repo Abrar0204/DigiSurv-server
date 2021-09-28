a5600a2fe269facfe102b0400750548ab64ae290
Abrar0204 <abrarhasan0204@gmail.com>
2021-09-28 10:54:55 +0530

    prod-test-2

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,22 @@ import { AppModule } from './app.module';
 
 import * as morgan from 'morgan';
 // import { PeerServer } from 'peer';
-// const whitelist = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'https://digi-surv-ui.vercel.app'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
 
-  // app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (!origin || whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  // });
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   app.use(morgan('dev'));
 
   // PeerServer({ port: 5500, path: '/' });
